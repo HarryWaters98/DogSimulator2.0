35cd0eadf7fab034776c47d43b81f465954dcd31
HarryWaters98 <39122567+HarryWaters98@users.noreply.github.com>
2018-12-12 23:24:54 +0000

    game update

---
+++ Assets/Scripts/OnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrigger : MonoBehaviour {
+
+    public GameObject Bone;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+         Scorescript.ScoreValue += 1;
+         Destroy(Bone);
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Scorescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorescript : MonoBehaviour {
+
+	public static int ScoreValue = 0;
+    Text Score;
+
+    public void Start()
+    {
+        Score = GetComponent<Text>();
+    }
+
+    public void Update()
+    {
+        Score.text = "Bones Collected : " + ScoreValue;
+    }
+}
